9c809c4b0c2c2b7a83392e98694f96a4a6eb2f46
Kevin <124392964+K3viiin@users.noreply.github.com>
2023-03-22 17:53:18 -0600

    Add files via upload

---
+++ conceptos.js
@@ -0,0 +1,17 @@
+// Elementos
+
+let logo = document.querySelector(".logo");
+
+const redirigir = ()=>{
+    location.href = "https://www.facebook.com/people/Avance-Insa/100013071965296/";
+}
+
+const darkMode = ()=>{
+    classList.add ()
+}
+
+// Codigo
+
+logo.addEventListener("click",(r)=>{
+    // redirigir();
+})
